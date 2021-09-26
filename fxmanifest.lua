8f2f2ce7af28772ce03874663ae758de8737367b
Simon <770632+Ponjimon@users.noreply.github.com>
2021-09-26 19:29:22 +0200

    chore: adapted script for redm

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'bodacious'
+
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+author 'EgoPvP.com (adapted by RedCatRP)'
+version '2.0.0'
+
+client_script "client.lua"
+ui_page 'html/index.html'
+
+
+files {
+	'html/index.html',
+	'html/script.js',
+}
